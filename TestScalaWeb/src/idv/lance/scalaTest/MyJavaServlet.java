89896e4b53e359a3329b39a464101ef8bd42ff7a
smoker21 <chen.lance@gmail.com>
2012-07-20 22:33:18 +0800

    Test to Use GitHub

---
+++ TestScalaWeb/src/idv/lance/scalaTest/MyJavaServlet.java
@@ -0,0 +1,38 @@
+package idv.lance.scalaTest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyJavaServlet
+ */
+@WebServlet("/MyJavaServlet")
+public class MyJavaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyJavaServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ TestScalaWeb/src/idv/lance/scalaTest/ScalaServlet.scala
@@ -0,0 +1,17 @@
+package idv.lance.scalaTest
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet
+
+@WebServlet(name = "scala", urlPatterns = Array("/myScalaServlet"))
+class ScalaServlet extends HttpServlet {
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+    val content: String = "<html><body><h1>Hello</h1></body></html>";
+    response.getWriter().println(content);
+  }
+
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse) {
+
+  }
+}
